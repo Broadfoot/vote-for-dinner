014ab58830c1a6e0cb0fdfc25628c9a4a746223c
Christopher Baek <baekc@amazon.com>
2017-01-19 15:28:11 -0800

    Add placeholders for dinner logic

--- public/vote-for-dinner.js
+++ public/vote-for-dinner.js
@@ -5,6 +5,7 @@
     var dinnersDatabaseReference = firebase.database().ref('dinners');
     var votesDatabaseReference = firebase.database().ref('votes');
 
+
     /**
      * TODO: Add an event listener (and the corresponding HTML to the
      * index.html) so users can "register" with their name.
@@ -13,8 +14,8 @@
      * 
      *     var userReference = usersDatabaseReference.push();
      *     userReference.set(YOUR_USER_OBJECT_GOES_HERE);
-     * 
      */
+
 
     /**
      * TODO: Whenever a new user is added to the database, this anonymous
@@ -39,6 +40,7 @@
         var userData = snapshot.val();
     });
 
+
     /**
      * TODO: Whenever a user is removed from the database, this anonymous
      * callback function will be called.
@@ -48,6 +50,64 @@
     usersDatabaseReference.on('child_removed', function(snapshot) {
         // userId will be unique for every user added
         var userId = snapshot.key;
+    });
+
+
+    /**
+     * TODO: Add an event listener (and the corresponding HTML to the
+     * index.html) so users can add new dinner options.
+     * 
+     * This is how you can create a new dinner option in the database;
+     * 
+     *     var dinnerReference = dinnersDatabaseReference.push();
+     *     dinnerReference.set(YOUR_DINNER_OBJECT_GOES_HERE);
+     */
+
+
+    /**
+     * TODO: Whenever a new dinner is added to the database, this anonymous
+     * callback function wil be called. This function is also called
+     * for every dinner in the dinners database when the app is first loaded.
+     * 
+     * When a dinner is added, add it to the DOM.
+     * 
+     * These may come in handy:
+     *   This is how to store a vote for a particular userId and dinnerId:
+     *     firebase.database().ref('votes/' + userId).set({dinnerId: dinnerId});
+     * 
+     *   This is how to remove a dinner from the database:
+     *     firebase.database().ref('dinners/' + dinnerId).remove();
+     * 
+     *   This is how to search the votes database and remove all votes for a
+     *   a particular dinnerId:
+     * 
+     *     votesDatabaseReference.once('value', function(votesSnapshot) {
+     *       votesSnapshot.forEach(function(voteSnapshot) {
+     *         if (voteSnapshot.val()['dinnerId'] === dinnerId) {
+     *           firebase.database().ref('votes/' + voteSnapshot.key).remove();
+     *         }
+     *       });
+     *     });
+     */
+    dinnersDatabaseReference.on('child_added', function(snapshot) {
+        // dinnerId will be unique for every dinner added
+        var dinnerId = snapshot.key;
+
+        // dinnerData will be the YOUR_DINNER_OBJECT_GOES_HERE object that was
+        // saved when the dinner was added
+        var dinnerData = snapshot.val();
+    });
+
+
+    /**
+     * TODO: Whenever a dinner is removed from the database, this anonymous
+     * callback function will be called.
+     * 
+     * When a dinner is removed, remove it from the DOM.
+     */
+    dinnersDatabaseReference.on('child_removed', function(snapshot) {
+        // dinnerId will be unique for every dinner added
+        var dinnerId = snapshot.key;
     });
 
 
